388681d9c2e6542be0dc62fa5ee0cb0be0ef3a92
Amber <maxbycode@gmail.com>
2017-06-17 13:18:09 -0600

    Added dependencies

---
+++ layout/head.php
@@ -0,0 +1,8 @@
+<title>v0lture Client</title>
+
+<!-- Resources -->
+<script src="js/jquery.min.js"></script>
+<script src="js/materialize.min.js"></script>
+<script src="js/app.js"></script>
+<link rel="stylesheet" href="css/materialize.min.css">
+<link rel="stylesheet" href="css/app.css">
